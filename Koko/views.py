533cc10613ef38e17f6b8bec1f55f67828666457
Markus Beuckelmann <email@markus-beuckelmann.de>
2014-08-21 17:47:38 +0200

    Add the landing page

--- Koko/views.py
+++ Koko/views.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+
+from flask import render_template
+from base import app
+
+@app.route('/', methods = ['GET'])
+@app.route('/koko', methods = ['GET'])
+def start():
+	''' Returns a landing page. '''
+
+	return render_template('start.html')
