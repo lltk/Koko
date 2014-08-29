beabf9d5ad8ff63f7965a97e8163be95e00af2bd
Markus Beuckelmann <email@markus-beuckelmann.de>
2014-08-29 15:51:17 +0200

    Bump version to 0.1.0

--- Koko/base.py
+++ Koko/base.py
@@ -3,7 +3,7 @@
 
 __author__ = 'Markus Beuckelmann'
 __author_email__ = 'email@markus-beuckelmann.de'
-__version__ = '0.0.1'
+__version__ = '0.1.0'
 
 from flask import Flask
 from flask.ext.cache import Cache
